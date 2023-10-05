3427c2a16e6b795489c38fec02217a11cee47003
Andrew M. Soltisz, PhD <andysoltisz@gmail.com>
2023-10-05 13:51:30 -0600

    Update isotropic_replacement.m

--- Isotropic Replacement/isotropic_replacement.m
+++ Isotropic Replacement/isotropic_replacement.m
@@ -18,7 +18,13 @@ function [isotropic_mask, isotropic_ROI, calibration_new] = isotropic_replacemen
 % 
 % Author: Andrew M. Soltisz
 % Email: andysoltisz@gmail.com
-% Last Updated: 05/18/2023
+% GitHub: https://github.com/andrewsoltisz/SPACE---Spatial-Pattern-Analysis-using-Closest-Events
+% Publication: https://doi.org/10.1101/2023.05.17.541131
+% Last Updated: 10/05/2023
+%
+% Copyright (C) 2023, Andrew Michael Soltisz. All rights reserved.
+% This source code is licensed under the BSD-3-Clause License found in the
+% LICENSE.txt file in the root directory of this source tree.
 
     %% Input Validation
 

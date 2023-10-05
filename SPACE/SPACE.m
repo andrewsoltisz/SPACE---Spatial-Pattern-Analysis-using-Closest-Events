948eec4acbbc1bf6ab08ee7a97b1d34aecab434f
Andrew M. Soltisz, PhD <andysoltisz@gmail.com>
2023-10-05 13:52:05 -0600

    Update SPACE.m

--- SPACE/SPACE.m
+++ SPACE/SPACE.m
@@ -19,7 +19,7 @@ function [varargout] = SPACE(X_mask, Y_mask, ROI_mask, pixel_size)
 % to each image and columns corresponding to the various single-image SPACE
 % analysis results.
 %
-% USES CASES:
+% USE CASES:
 % 
 % [...] = SPACE(X_mask, Y_mask) perform analysis without specifiying an
 % ROI or pixel size. Here, the ROI is treated as the entire image and the
@@ -51,7 +51,13 @@ function [varargout] = SPACE(X_mask, Y_mask, ROI_mask, pixel_size)
 % Spatial Pattern Analysis using Closest Events (SPACE)
 % Author: Andrew M. Soltisz
 % Email: andysoltisz@gmail.com
-% Last Updated: 05/18/2023
+% GitHub: https://github.com/andrewsoltisz/SPACE---Spatial-Pattern-Analysis-using-Closest-Events
+% Publication: https://doi.org/10.1101/2023.05.17.541131
+% Last Updated: 10/05/2023
+%
+% Copyright (C) 2023, Andrew Michael Soltisz. All rights reserved.
+% This source code is licensed under the BSD-3-Clause License found in the
+% LICENSE.txt file in the root directory of this source tree.
 
     % Check for appropriate number of inputs
     if nargin < 2
